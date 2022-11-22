56e431d77084c7c4f1fc09448f3da61f8e1d83d5
Coding4usall <112630854+Coding4usall@users.noreply.github.com>
2022-11-22 17:49:20 -0300

    [VAL]00011 FEATURE/Add mask to price field

--- js/app.js
+++ js/app.js
@@ -3,6 +3,22 @@ import { valida } from "./validacao.js";
 const inputs = document.querySelectorAll("input");
 
 inputs.forEach((input) => {
+
+if(input.dataset.tipo == 'preco'){
+
+  SimpleMaskMoney.setMask(input, {
+    allowNegative: false,
+    negativeSignAfter: false,
+    prefix: 'R$',
+    fixed: true,
+    fractionDigits: 2,
+    decimalSeparator: ',',
+    thousandsSeparator: '.',
+    cursor: 'end'
+  });
+
+}
+
   input.addEventListener("blur", (e) => {
     console.log(e.target)
     valida(e.target);
